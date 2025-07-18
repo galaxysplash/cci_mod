d507ba7cb4662cf1a155dd507839af14c8a45c96
Galaxy Master <Marcel.Hajek@gmx.de>
2025-07-18 18:46:04 +0200

    ...

---
+++ build/CMakeFiles/cci_mod.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cci_mod.dir/src/main.cpp.o"
+  "CMakeFiles/cci_mod.dir/src/main.cpp.o.d"
+  "cci_mod"
+  "cci_mod.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cci_mod.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/cci_mod.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cci_mod.
+# This may be replaced when dependencies are built.
